55ec78cc0851e67195b6663ea16c6358dead365f
alban_old_ubuntu <old_ubuntu@mail.com>
2023-11-20 11:17:46 +0100

    neo4j - fixed consumer

--- fill_db.py
+++ fill_db.py
@@ -15,6 +15,8 @@ class Article:
     def __init__(self, _id, title):
         self._id=_id
         self.title=title
+    def to_string(self):
+            print(str(self._id)+" "+str(self.title))
 
 class Author:
     def __init__(self, _id, name):
@@ -25,9 +27,9 @@ class Consumer:
 
     def __init__(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
-        self.tx_limit=100
-        self.citation=[]
-        self.authored=[]
+        self.tx_limit=1
+        self.citation=[] # to keep tracks, delete later
+        self.authored=[] # to keep tracks, delete later
         self.tmp_article_array = []
         self.tmp_author_array = []
 
@@ -43,14 +45,21 @@ class Consumer:
                 self.citation.append((tmp_article._id,references))
         if(authored_flag):
             for author in line_dict["authors"]:
-                self.authored.append((author.get("_id",None),tmp_article._id))
+                if not '_id' in author:
+                        print("missing id")
+                        break
+                tmp_author = Author(author["_id"],author.get("name", None))
+                    self.insert_author(tmp_author)
+                    self.authored.append((author.get("_id",None),tmp_article._id))
 
         
         
 
     def close(self):
-        self.insert_articles(self.tmp_article_array)
-        self.insert_authors(self.tmp_author_array)
+        if (len(self.tmp_article_array) != 0):
+            self.insert_articles(self.tmp_article_array)
+        if (len(self.tmp_author_array) != 0):
+            self.insert_authors(self.tmp_author_array)
         self.driver.close()
 
 
@@ -84,7 +93,8 @@ class Consumer:
             tx_query = ""
             for article in articles:
                 tx_query += f"CREATE (a:Article {{_id: '{article._id}',title: '{article.title}'}}),"
-            
+
+            #print(tx_query[:-1])
             result = session.write_transaction(self._run_query, tx_query[:-1]) # on enleve la derniere virgule
             print(result)
 
@@ -93,9 +103,21 @@ class Consumer:
         with self.driver.session() as session:
             tx_query = ""
             for author in authors:
-                tx_query += f"CREATE (a:Author {{_id: '{author._id}',name: '{author.name}'}}),"
-            
+                if(author.name is not None):
+                    tx_query += f"MERGE (a:Author {{_id: '{author._id}',name: '{author.name}'}})," # MERGE to avoid doublons
+                else:
+                    tx_query += f"MERGE (a:Author {{_id: '{author._id}'}})," # MERGE to avoid doublons
+
             result = session.write_transaction(self._run_query, tx_query[:-1]) # on enleve la derniere virgule
+            print(result)
+
+    def flush_db(self):
+        with self.driver.session() as session:
+            tx_query1 = f"MATCH (a) -[r] -> () DELETE a, r"
+            tx_query2 = f"MATCH (a) DELETE a"
+
+            result = session.write_transaction(self._run_query, tx_query1)
+            result = session.write_transaction(self._run_query, tx_query2)
             print(result)
 
     @staticmethod
